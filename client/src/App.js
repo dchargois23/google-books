9665d84675197998f8e1523e3d4a7013e2235959
Derrick Chargois <36555200+dchargois23@users.noreply.github.com>
2020-07-15 10:11:03 -0500

    componenet push

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,73 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Search from './components/Search/Search';
+import Display from './components/Display/Display';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
+const searchURL = 'https://www.googleapis.com/books/v1/volumes?';
+const APIKey = 'AIzaSyCgoa5LezjjjY6hOUkXJUjC7h0lswQrwWs';
+
+class App extends React.Component {
+  state = {
+    results: [],
+    searchText: '',
+    printType: 'all',
+    bookType: 'ebooks',
+  }
+
+  displayBooks = data => {
+    this.setState({
+      results: [data]
+    });
+  }
+
+  handleChange = e => {
+    const {
+      target: { name, value }
+    } = e;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.fetchBooks();
+  }
+
+  fetchBooks = () => {
+    const URL = `${searchURL}q=${this.state.searchText}&filter=${this.state.bookType}&printType=${this.state.printType}&langRestrict=en&orderBy=relevance&key=${APIKey}`;
+
+    fetch(URL)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Something is wrong, please try again later.');
+        }
+        return response;
+      })
+      .then(response => response.json())
+      .then(data => this.displayBooks(data))
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      })
+  };
+
+  render() {
+    return (
+      <main className='App'>
+        <Header />
+        <Search
+          displayBooks={data => this.displayBooks(data)}
+          handleSubmit={this.handleSubmit}
+          searchText={this.state.searchText}
+          handleChange={this.handleChange}
+          printType={this.state.printType}
+          bookType={this.state.bookType}
+        />
+        <Display results={this.state.results} />
+      </main>
+    );
+  }
 }
-
 
 export default App;
